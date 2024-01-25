20bd12a7bcb1d3fbbb8e5fd8674960a85754f94f
Tary131 <130067664+Tary131@users.noreply.github.com>
2024-01-25 22:52:01 +0100

    Add registration form

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,28 @@ import { useState, Suspense } from "react";
 import Header from "./components/Header/Header.jsx";
 import ShoppingList from "./components/ShoppingList/ShoppingList.jsx";
 import AllLists from "./components/AllLists/AllLists.jsx";
+import {Registration} from "./components/Registration/Registration.jsx";
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme, darkTheme } from "./theme.js";
 import i18n from "i18next";
 import Login from "./components/Login/Login.jsx"
+import styles from "./App.module.css";
 
 
 const App = () => {
 	const [darkMode, setDarkMode] = useState(false);
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [showRegistration, setShowRegistration] = useState(false);
 	const toggleDarkMode = () => {
 		setDarkMode((prevMode) => !prevMode);
 	};
-
+	const handleCreateAccountClick = () => {
+		setShowRegistration(true);
+	};
 	return (
-		<div>
+		<div  className={`${styles.app} ${darkMode ? styles.darkMode : ""}`}>
+
 			<Suspense fallback="loading...">
 				<ThemeProvider theme={darkMode ? darkTheme : theme}>
 					<Header
@@ -25,6 +31,9 @@ const App = () => {
 						darkMode={darkMode}
 						i18n={i18n}
 						availableLanguages={["en", "ru"]}
+						isAuthenticated={isAuthenticated}
+						setIsAuth={setIsAuthenticated}
+						onCreateAccountClick={handleCreateAccountClick}
 					/>
 					{isAuthenticated ? (
 					<Routes>
@@ -42,9 +51,10 @@ const App = () => {
 						/>
 
 					</Routes>) : (
-
+<>
 						<Login setIsAuth={setIsAuthenticated} />
-						)}
+	{showRegistration && <Registration  />}
+</>)}
 				</ThemeProvider>
 			</Suspense>
 		</div>
